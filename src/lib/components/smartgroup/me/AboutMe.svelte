1d036206c32ac85459fdeabac9f37a77c03e9103
Boris <23BOsmanov@heckgrammar.co.uk>
2025-03-16 17:02:16 +0000

    Added cool effect and socials

--- src/lib/components/smartgroup/me/AboutMe.svelte
+++ src/lib/components/smartgroup/me/AboutMe.svelte
@@ -3,13 +3,13 @@
   import { cubicInOut } from "svelte/easing";
   import Person from "$lib/icons/Person.svelte";
   import { blur, scale } from "svelte/transition";
-  import { onMount } from "svelte";
+  import NumberFlow from '@number-flow/svelte'
 
   let isDivVisible = false;
 
   let age = 13;
   let languages = 4;
-  let hours = 528;
+  let hours = 531;
   let school = 75; // New variable for the slider
   const ageWidth = tweened(0, { duration: 1000, easing: cubicInOut });
   const languagesWidth = tweened(0, { duration: 1000, easing: cubicInOut });
@@ -39,7 +39,7 @@
     event: MouseEvent,
     property: string,
     min: number,
-    max: number,
+    max: number
   ) {
     const progressBar = event.target as HTMLElement;
     const rect = progressBar.getBoundingClientRect();
@@ -63,7 +63,7 @@
     event: TouchEvent,
     property: string,
     min: number,
-    max: number,
+    max: number
   ) {
     const progressBar = event.target as HTMLElement;
     const rect = progressBar.getBoundingClientRect();
@@ -141,7 +141,9 @@
                     style="width: {$ageWidth}%"
                   >
                     <span transition:blur={{ duration: 300, delay: 400 }}
-                      >{age}</span
+                      >
+                      <NumberFlow value={age} />
+                      </span
                     >
                     <span
                       transition:blur={{ duration: 300, delay: 475 }}
@@ -172,7 +174,7 @@
                     style="width: {$languagesWidth}%"
                   >
                     <span transition:blur={{ duration: 300, delay: 400 }}
-                      >{languages}</span
+                      ><NumberFlow value={languages} /></span
                     >
                     <span
                       transition:blur={{ duration: 300, delay: 475 }}
@@ -203,7 +205,7 @@
                     style="width: {$hoursWidth}%"
                   >
                     <span transition:blur={{ duration: 300, delay: 400 }}
-                      >{hours}</span
+                      > <NumberFlow value={hours} /> </span
                     >
                     <span
                       transition:blur={{ duration: 300, delay: 475 }}
@@ -234,7 +236,7 @@
                     style="width: {$schoolWidth}%"
                   >
                     <span transition:blur={{ duration: 300, delay: 400 }}
-                      >{school}</span
+                      > <NumberFlow value={school} /> </span
                     >
                     <span
                       transition:blur={{ duration: 300, delay: 475 }}
@@ -251,10 +253,16 @@
             </div>
             <div class="space-y-2">
               <!-- PART 2: Socials -->
-              <span class="text-xs opacity-40"> PLAYER ACTIONS </span>
+              <span class="text-xs opacity-40"> PLAYER SOCIALS </span>
               <div class="flex flex-col gap-2">
                 <ul class="flex flex-row gap-2">
-                  <li class="flex items-center p-2 rounded-lg border border-[#23272A] bg-[#454FBF]/30 h-10 w-10">
+                  <button
+                    aria-label="Discord"
+                    on:click={() =>
+                      (window.location.href =
+                        "https://discord.com/users/991742998025089106")}
+                    class="p-2 rounded-lg transition-all border border-[#454FBF] bg-[#454FBF]/20 focus:bg-[#454FBF] h-10 w-10"
+                  >
                     <svg
                       viewBox="0 0 64 48"
                       fill="none"
@@ -265,16 +273,75 @@
                         fill="white"
                       />
                     </svg>
-                  </li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                </ul>
-                <ul class="flex flex-row gap-2">
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
-                  <li class="p-2 rounded-lg bg-purple-500/70 h-10 w-10">d</li>
+                  </button>
+                  <button
+                    aria-label="Telegram"
+                    on:click={() =>
+                      (window.location.href =
+                        "https://t.me/thebosmanov")}
+                    class="p-2 rounded-lg transition-all border border-white/70 bg-white/20 focus:bg-white h-10 w-10"
+                  >
+                    <svg
+                      class="h-full"
+                      viewBox="0 0 1000 1000"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                    >
+                      <defs>
+                        <linearGradient
+                          x1="50%"
+                          y1="0%"
+                          x2="50%"
+                          y2="99.2583404%"
+                          id="linearGradient-1"
+                        >
+                          <stop stop-color="#2AABEE" offset="0%"></stop>
+                          <stop stop-color="#229ED9" offset="100%"></stop>
+                        </linearGradient>
+                      </defs>
+                      <g
+                        id="Artboard"
+                        stroke="none"
+                        stroke-width="1"
+                        fill="none"
+                        fill-rule="evenodd"
+                      >
+                        <circle
+                          id="Oval"
+                          fill="url(#linearGradient-1)"
+                          cx="500"
+                          cy="500"
+                          r="500"
+                        ></circle>
+                        <path
+                          d="M226.328419,494.722069 C372.088573,431.216685 469.284839,389.350049 517.917216,369.122161 C656.772535,311.36743 685.625481,301.334815 704.431427,301.003532 C708.567621,300.93067 717.815839,301.955743 723.806446,306.816707 C728.864797,310.92121 730.256552,316.46581 730.922551,320.357329 C731.588551,324.248848 732.417879,333.113828 731.758626,340.040666 C724.234007,419.102486 691.675104,610.964674 675.110982,699.515267 C668.10208,736.984342 654.301336,749.547532 640.940618,750.777006 C611.904684,753.448938 589.856115,731.588035 561.733393,713.153237 C517.726886,684.306416 492.866009,666.349181 450.150074,638.200013 C400.78442,605.66878 432.786119,587.789048 460.919462,558.568563 C468.282091,550.921423 596.21508,434.556479 598.691227,424.000355 C599.00091,422.680135 599.288312,417.758981 596.36474,415.160431 C593.441168,412.561881 589.126229,413.450484 586.012448,414.157198 C581.598758,415.158943 511.297793,461.625274 375.109553,553.556189 C355.154858,567.258623 337.080515,573.934908 320.886524,573.585046 C303.033948,573.199351 268.692754,563.490928 243.163606,555.192408 C211.851067,545.013936 186.964484,539.632504 189.131547,522.346309 C190.260287,513.342589 202.659244,504.134509 226.328419,494.722069 Z"
+                          id="Path-3"
+                          fill="#fff"
+                        ></path>
+                      </g>
+                    </svg>
+                  </button>
+                  <button aria-label="Email" on:click={() => window.location.href = "mailto:boris@0boris.xyz"} class="p-2 rounded-lg transition-all border border-green-500/70 bg-green-500/20 focus:bg-green-500 h-10 w-10"
+                    ><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                      ><path
+                        d="M22 8.608v8.142a3.25 3.25 0 0 1-3.066 3.245L18.75 20H5.25a3.25 3.25 0 0 1-3.245-3.066L2 16.75V8.608l9.652 5.056a.75.75 0 0 0 .696 0L22 8.608ZM5.25 4h13.5a3.25 3.25 0 0 1 3.234 2.924L12 12.154l-9.984-5.23a3.25 3.25 0 0 1 3.048-2.919L5.25 4h13.5-13.5Z"
+                        fill="#fff"
+                      /></svg
+                    ></button
+                  >
+                  <button aria-label="X" on:click={() => window.location.href = "https://x.com/0boris_"} class="p-2 rounded-lg border border-black/70 bg-black/20 focus:bg-black h-10 w-10"
+                    ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-full fill-white"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
+                    />
+                  </svg>
+                  </button
+                  >
                 </ul>
               </div>
             </div>
